0d063d4a34ba528b8adf2e94b5288774c46943b4
Robb Böhnke <212465+robb@users.noreply.github.com>
2023-01-18 14:51:31 +0100

    0.2.0 (#15)
    Co-authored-by: MVP Bot <bot@movingparts.io>
    Co-authored-by: Kasper Lahti <kasper@movingparts.io>

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pow",
-            url: "https://packages.movingparts.io/binaries/pow/0.1.1/Pow.xcframework.zip",
-            checksum: "5ae07875130ecb40d9310460b13da6aaa6f783d1e6e9af88b7c96a1ba32fd22a"
+            url: "https://packages.movingparts.io/binaries/pow/0.2.0/Pow.xcframework.zip",
+            checksum: "a3b2f1b8714002631293dec6087068017e1b3744f4d462c99a5c385abcbcd600"
         ),
     ]
 )
