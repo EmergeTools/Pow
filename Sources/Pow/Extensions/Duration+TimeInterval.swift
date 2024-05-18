f9d979bcddcaa9d2ed4d42ec9a40c99c8e141322
Adam McNight <mcnight@mcnight.fr>
2024-05-18 14:05:25 +0200

    Add tvOS support

--- Sources/Pow/Extensions/Duration+TimeInterval.swift
+++ Sources/Pow/Extensions/Duration+TimeInterval.swift
@@ -2,6 +2,7 @@ import Foundation
 
 @available(iOS 16.0, *)
 @available(macOS 13.0, *)
+@available(tvOS 16.0, *)
 internal extension Duration {
     var timeInterval: TimeInterval {
         TimeInterval(components.seconds) + TimeInterval(components.attoseconds) / 1e18
